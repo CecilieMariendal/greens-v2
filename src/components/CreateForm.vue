c7c2ed51aa87f6cd76678cbbd8c6201e42198a3d
Cecilie Mariendal <ctmariendal@gmail.com>
2021-06-20 12:50:12 +0200

    wip create page

--- src/components/CreateForm.vue
+++ src/components/CreateForm.vue
@@ -7,20 +7,24 @@
     <p>Or</p>
     <form>
         <h2>Create new recipe</h2>
-        <fieldset>
-            <legend>Base information</legend>
-            <input type="text" name="Title" placeholder="Name of the recipe...">
-            <textarea name="description" cols="30" rows="10" placeholder="Write a description"></textarea>
-            <input type="file" name="image" accept="image/png, image/gif, image/jpeg">
-        </fieldset>
-        <fieldset>
-            <legend>Ingredients</legend>
+        <div class="basic">
+            <label for="image">
+                <img src="recipe-placeholder.jpg" alt="banner image">
+                <input type="file" id="image" name="image" accept="image/png, image/gif, image/jpeg">
+            </label>
+            <div>
+                <input v-model="headline" type="text" name="title" placeholder="Name of the recipe...">
+                <textarea v-model="content" name="description" cols="30" rows="10" placeholder="Write a description"></textarea>
+            </div>
+        </div>
+        <div class="fieldset">
+            <h3>Ingredients</h3>
             <input type="text" name="Ingredient">
-        </fieldset>
-        <fieldset>
-            <legend>Steps</legend>
+        </div>
+        <div class="fieldset">
+            <h3>Steps</h3>
             <input type="text" name="step">
-        </fieldset>
+        </div>
         <button type="submit">Submit</button>
     </form>
 </template>
@@ -31,16 +35,74 @@
         data() {
           return {
               url: '',
+              portrait: '',
+              headline: '',
+              content: '',
+              ingredients: [],
+              steps: [],
           }
         },
         methods: {
             onsubmit() {
+                
                 // TODO: Import recipe
             },
         },
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    form {
+        div {
+            &.basic {
+                @media only screen and (min-width: 700px) {
+                    display: grid;
+                    grid-template-columns: 500px 1fr;
+                    grid-gap: 1vw;
+                }
 
+                label {
+                    display: block;
+                    width: 100%;
+                    height: 300px;
+                    overflow: hidden;
+                    
+                    input { display: none; }
+                    img {
+                        width: 100%;
+                        height: 100%;
+                        object-fit: cover;
+                    }
+                }
+
+                > div {
+                    display: flex;
+                    flex-direction: column;
+
+                    input[name="title"] {
+                        font-size: 1.5em;
+                        margin-bottom: 1em;
+                    }
+
+                    textarea {
+                        height: 100%;
+                        border: none;
+                    }
+                }
+            }
+
+            input {
+                width: 100%;
+                box-sizing: border-box;
+                border-radius: 0;
+                border: none;
+                border-bottom: 2px solid silver;
+
+                &:focus {
+                    outline: none;
+                    border-bottom-color: black;
+                }
+            }
+        }
+    }
 </style>
